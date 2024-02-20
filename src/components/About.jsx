732272c93a2e508ceb8219cbd5ae3031847a79e0
Mr.Boss <vasawat77@gmail.com>
2024-02-20 22:03:58 +0700

    committ

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,20 @@
-import aboutimg from "../images/about.svg";
 import "./about.css";
 
 export default function About() {
   return (
     <div className="container">
       <h2 className="title">about us</h2>
-      <img className="bigImg" src={aboutimg} alt="" />
+      <div className="rickrollBox">
+        <iframe
+          title="rickroll"
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=b2zlCE5jn66BrKy?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1"
+          frameborder="0"
+          allow="autoplay"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 }
